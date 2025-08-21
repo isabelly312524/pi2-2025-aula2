db8029c9275d211869e1fb4c14154c5df6b278b3
isabelly312524 <isabellygiovanabatistadasilva@gmail.com>
2025-08-21 23:40:18 +0000

    Aula 21/08/25

---
+++ public/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Página principal</title>
+</head>
+<body>
+    <h1>Página principal</h1>
+    <p>Primeiro exemplo de página dinâmica</p>
+    <?php
+    include_once('./includes/teste.php');
+    
+    ?>
+</body>
+</html>
